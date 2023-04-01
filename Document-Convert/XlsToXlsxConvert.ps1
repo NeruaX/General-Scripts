a69914124e80509c7fa8b7bf30440453ae09528b
NeruaX <128629116+NeruaX@users.noreply.github.com>
2023-04-01 10:51:36 -0700

    Update XlsToXlsxConvert.ps1

--- Document-Convert/XlsToXlsxConvert.ps1
+++ Document-Convert/XlsToXlsxConvert.ps1
@@ -20,12 +20,12 @@ Get-ChildItem -Path $RootFolder -Include $FileTypeOld -Recurse | ForEach-Object 
     $xls = $_
     $FileName = $xls.Name
     $Path = $xls.FullName
-    #Setup Archive information
-    $ArchiveSubFolder = $ArchiveFolder+$Path.Substring($RootFolderLength, $Path.Length-$RootFolderLength-$FileName.Length)
-    $ArchivePath = $ArchiveSubFolder+$FileName
     
     if($ArchiveXls) {
-    #Archives original
+        #Setup Archive information
+        $ArchiveSubFolder = $ArchiveFolder+$Path.Substring($RootFolderLength, $Path.Length-$RootFolderLength-$FileName.Length)
+        $ArchivePath = $ArchiveSubFolder+$FileName
+        #Archives original
         if(-not(Test-Path -PathType Container $ArchiveSubFolder)) {
             New-Item -item Directory -Force -Path $ArchiveSubFolder | Out-Null
         }
